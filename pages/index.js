5f130a222e112d6d0d11e48cae0ff48d165de519
Agustí Moll <agusti@gmail.com>
2020-05-16 21:02:16 +0200

    first release

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function IndexPage() {
+    return (
+            <div>
+                <p>
+                    ¡Este es mi primer deploy con Next.js!
+                </p>
+            </div>
+    )
+}
+export default IndexPage
